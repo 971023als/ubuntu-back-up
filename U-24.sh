c6c1b0cafaf7ca2c3852b2d37ca620e4f4820434
971023als <971023als@naver.com>
2023-02-12 01:30:40 +0900

    Update U-24.sh

--- U-24.sh
+++ U-24.sh
@@ -16,22 +16,22 @@ EOF
 
 BAR
 
-
-
-# Check if NFS service is installed
+# NFS 서비스가 설치되어 있는지 확인합니다
 if ! command -v nfsd &> /dev/null; then
-  echo "NFS service not installed. Aborting restore."
+  INFO "NFS 서비스가 설치되지 않았습니다. 복원을 중단하는 중입니다."
+else
+  INFO "NFS 서비스가 설치되었습니다. 복원 중입니다."
 fi
 
-# Restore shares in dfstab or exports
+# dfstab 또는 내보내기에서 공유 복원
 if [ -f "/etc/dfs/dfstab.bak" ]; then
   cp "/etc/dfs/dfstab.bak" "/etc/dfs/dfstab"
-  echo "Shares restored in /etc/dfs/dfstab."
+  INFO "/etc/dfs/dfstab에서 복원된 공유"
 elif [ -f "/etc/exports.bak" ]; then
   cp "/etc/exports.bak" "/etc/exports"
-  echo "Shares restored in /etc/exports."
+  INFO "/etc/exports에서 복원된 공유."
 else
-  echo "Shares backup file not found."
+  INFO "공유 백업 파일을 찾을 수 없습니다."
 fi
 
 # Start NFS services
@@ -40,12 +40,21 @@ services=("nfsd" "statd" "mountd")
 for service in "${services[@]}"; do
   service "$service" start
   if [ $? -eq 0 ]; then
-    echo "$service started successfully."
-  else
-    echo "Failed to start $service."
+    INFO "$service 가 성공적으로 시작되었습니다."
+  else 
+    INFO "$service 를 시작하지 못했습니다."
   fi
 done
 
+# 사용하지 않도록 설정된 NFS 시작 스크립트의 이름을 원래 이름으로 변경합니다
+if [ -f "/etc/rc.d/rc2.d/_S60nfs" ]; then
+sudo mv /etc/rc.d/rc2.d/_S60nfs /etc/rc.d/rc2.d/S60nfs
+fi
+
+# NFS 관련 프로세스 시작
+/usr/sbin/nfsd restart
+/usr/sbin/statd restart
+/usr/sbin/lockd restart
 
 
 cat $result
