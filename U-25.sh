f0982f313b8b1732d4337467a1e54c5c6f17fc45
971023als <971023als@naver.com>
2023-02-12 01:41:23 +0900

    Update U-25.sh

--- U-25.sh
+++ U-25.sh
@@ -18,24 +18,23 @@ EOF
 
 BAR
 
-# Start the NFS service
-sudo service nfs start
+# NFS 서비스 시작
+service nfs start
 
-# Check the status of the NFS service
-sudo service nfs status
+# NFS 서비스의 상태를 확인합니다
+service nfs status
 
-sudo mount 192.168.0.100:/nfs_share /mnt/nfs
+mount 192.168.0.100:/nfs_share /mnt/nfs
 
-# Check if the NFS share is mounted correctly by accessing it
+# NFS 공유에 액세스하여 NFS 공유가 올바르게 마운트되었는지 확인합니다
 ls /mnt/nfs
 
-# If there's an issue, print an error message and exit
+# 문제가 있는 경우 오류 메시지를 인쇄하고 종료합니다
 if [ $? -ne 0 ]; then
-  echo "Error: NFS service could not be restored to its original state"
+  INFO "NFS 서비스를 원래 상태로 복원할 수 없습니다."
 else
-  echo "NFS service has been successfully restored to its original state"
+  INFO "NFS 서비스가 성공적으로 원래 상태로 복원되었습니다."
 fi
-
 
 
 cat $result
